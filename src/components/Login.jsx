551d6e67a10ee72fa454755519b34ac5367071c4
Shady7781 <troudishedy6@gmail.com>
2021-08-06 00:20:32 +0200

    improve register design

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -10,8 +10,18 @@ function Login() {
           src="https://music-b26f.kxcdn.com/wp-content/uploads/2017/06/635963274692858859903160895_spotify-logo-horizontal-black.jpg"
           alt="Spotify logo"
         />
+
+        <form className="form">
+        <input className="form-input" type="email" placeholder="Enter your Email "></input>
+        <input className="form-input" type="password" placeholder="Enter your Password "></input>
+        <input className="form-input" type="password" placeholder="confirm your Password"></input>
+        <br></br>
+        <button type="button" className="login-with-google-btn" >Sign in with Google
+</button>
+        </form>
         <a href={loginUrl}>LOGIN WITH SPOTIFY</a>
       </div>
+      
     );
 }
 
